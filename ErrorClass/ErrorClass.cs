7480650d70101917b3117dffb328425fb61f1d8e
BlueSurfRhino <richowens@outlook.com>
2014-06-09 19:18:32 -0700

    Copy of all work completed on Saturday, June 7, 2014 at 4:27PM.
    All files and projects.  No integration.

---
+++ ErrorClass/ErrorClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ErrorClass
+{
+    public class ErrorClass
+    {
+        private string recipeerror;   // the name field
+        public string RecipeError   // the Name property
+        {
+            get{ return recipeerror;}
+            set { recipeerror = value; }
+        }
+
+
+        void LogError(string errormsg, object UIControl)
+        {
+            //method to log error
+            //UIControl.Text = errormsg;
+        }
+    }
+}
---
+++ RecipeClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApplication1
+{
+    class Recipe
+    {
+        public List <string>  ingredients {get; set;}
+        public string RecipeName { get; set; }
+        public string RecipeDirections { get; set; }
+        public string Comments { get; set; }
+        public string RecipeType { get; set; }
+        public string Yeild { get; set; }
+        public string ServingSize { get; set; } 
+    }
+    
+}
