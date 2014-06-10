1c575403906f24514d23d91ee29cebd4e3d297bf
BlueSurfRhino <richowens@outlook.com>
2014-06-09 20:09:53 -0700

    Integrated Recipes Error Class
    Integrated Rich's work by adding his class library for RecipeErrors and
    logging errors to UI.
    Noticed - UI was missing label for error logging.

---
+++ RecipesIntegrated/ErrorClass/ErrorClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipeErrorClass
+{
+    public static class RecipeErrorClass
+    {
+        private static string recipeerror;   // the name field
+        public static string RecipeError   // the Name property
+        {
+            get{ return recipeerror;}
+            set { recipeerror = value; }
+        }
+
+        public static void ErrorLogger()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
